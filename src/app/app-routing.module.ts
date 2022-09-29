274d2ebbb0c34b38db810fdeed94f80f01b71dfd
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-29 20:00:41 +0200

    add first part of project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './features/cart/cart.component';
+import { LoginComponent } from './features/login/login.component';
+import { SearchComponent } from './features/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: 'search' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
