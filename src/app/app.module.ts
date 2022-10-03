80baedee8e300493bc7d63857c3e0b86845e2441
Anita Liberatore <anitaliberatore@hotmail.com>
2022-10-03 16:31:15 +0200

    add first api on search panel for searching hotel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './features/search/search.component';
 import { LoginComponent } from './features/login/login.component';
 import { CartComponent } from './features/cart/cart.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { CartComponent } from './features/cart/cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Hotel } from '../../model/hotel';
+
+
 
 @Component({
   selector: 'app-search',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  text = 'Milano'
+  hotels: Hotel[] | undefined;  
+  
+  constructor(private http: HttpClient) {  
+  }
 
   ngOnInit(): void {
+    this.searchHotels(this.text)
+  }
+
+  searchHotels(text: string) {
+    this.text = text;
+    this.http.get<Hotel[]>('http://localhost:3000/hotels?q=' + text)
+    .subscribe(result => {
+      this.hotels = result;
+      console.log(this.hotels)
+    });
   }
 
 }
